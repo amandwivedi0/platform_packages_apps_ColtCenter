5a7d08ef4386503c8b50daafd4a9cbd011f88bc5
Dragon-oniv <nvrevrgivup@gmail.com>
2018-12-07 17:13:49 +0530

    Status bar clock customization [2/2]

--- src/com/colt/settings/fragments/statusbartabs/ClockOptions.java
+++ src/com/colt/settings/fragments/statusbartabs/ClockOptions.java
@@ -16,46 +16,39 @@
 
 package com.colt.settings.fragments.statusbartabs;
 
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.res.Resources;
+
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.Settings;
-import android.support.v7.preference.ListPreference;
+//import android.support.v7.preference.PreferenceCategory;
+//import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.PreferenceScreen;
+//import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
+//import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.Utils;
 
-public class ClockOptions extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+import com.android.internal.logging.nano.MetricsProto;
+
+//import com.citrus.settings.preference.SystemSettingSwitchPreference;
+
+public class ClockSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.clock_settings);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.COLT;
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return true;
-    }
-
 }
