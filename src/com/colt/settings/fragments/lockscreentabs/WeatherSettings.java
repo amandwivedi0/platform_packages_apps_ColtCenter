52ec72ba9b4d2209ba054846c6758d0a10be838a
Dragon-oniv <nvrevrgivup@gmail.com>
2018-12-07 12:27:25 +0530

    Integrate Weather client

--- src/com/colt/settings/fragments/lockscreentabs/WeatherSettings.java
+++ src/com/colt/settings/fragments/lockscreentabs/WeatherSettings.java
@@ -16,46 +16,66 @@
 
 package com.colt.settings.fragments.lockscreentabs;
 
+
+import android.app.ActivityManagerNative;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.provider.Settings;
-import android.support.v7.preference.ListPreference;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.Utils;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.colt.settings.preferences.SystemSettingSwitchPreference;
+import com.android.internal.util.custom.weather.WeatherClient;
 
-public class WeatherSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+import com.colt.settings.R;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class WeatherSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
-        addPreferencesFromResource(R.xml.weather_settings);
-    }
+	public static final String TAG = "LockScreenSettings";
+        private static final String WEATHER_LS_CAT = "weather_lockscreen_key";
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+    	    super.onCreate(savedInstanceState);
+
+	addPreferencesFromResource(R.xml.weather_settings);
+
+	final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        final PreferenceCategory weatherCategory = (PreferenceCategory) prefSet
+                 .findPreference(WEATHER_LS_CAT);
+
+         if (!WeatherClient.isAvailable(getContext())) {
+             prefSet.removePreference(weatherCategory);
+         }
+
+	}
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.COLT;
+	return MetricsProto.MetricsEvent.COLT;
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return true;
-    }
-
 }
